158e9387f9fbb6ee78fbbeaa80dd2f01ee933295
DeveloperGY <dev.garrettyenojr@gmail.com>
2021-09-25 12:59:43 -0400

    Framework Rework Fresh Start

--- src/framework/Engine.hpp
+++ src/framework/Engine.hpp
@@ -1,6 +1,5 @@
 #pragma once
 
-// Engine Framework Namespace
 namespace ef
 {
 	class Engine
--- src/framework/Renderer.hpp
+++ src/framework/Renderer.hpp
@@ -8,31 +8,8 @@ namespace ef
 	class Renderer
 	{
 	private:
-		sf::RenderWindow window;
-
-		int width;
-		int height;
-		std::string title;
 
 	public:
-		// Renderer Constructor
-		Renderer();
-
-		/* 	Renderer Initializer
-		*	@return Returns 0 if it succeedes
-		*/
-		int init();
-
-		/* Updates the Renderer
-		*	@return Returns 0 if it succeedes
-		*/
-		int update();
-
-		void render(); // Does NOT take an Entity, takes the parts of the entity required for rendering
-
-		// Getters/Setters
-
-		// Sets the Window Title
-		void setTitle(std::string);
+		
 	};
 }
